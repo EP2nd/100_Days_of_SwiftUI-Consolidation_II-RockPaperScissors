4454bcaf80caec7de2080760028acc58177a8046
EPJr <edwin.przezwiecki.jr@icloud.com>
2022-11-26 17:33:51 +0100

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Edwin Przeźwiecki Jr. on 26/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
