60cfe9fdac102a2a71c58fefc9f8f37ab755d0a2
EPJr <edwin.przezwiecki.jr@icloud.com>
2022-11-26 22:25:20 +0100

    Almost complete
    What is left to implement is the "game over" alert shown after 10 turns, and some possible additional UI improvements.

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,14 +8,91 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private var shapes = ["🪨", "🧻", "✂️"]
+    
+    @State private var confrontingShape = Int.random(in: 0 ..< 3)
+    @State private var winOrLose = Bool.random()
+    
+    @State private var score = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(stops: [
+                .init(color: .black, location: 0),
+                .init(color: .white, location: 0.7),
+                .init(color: .red, location: 1)
+            ], startPoint: .top, endPoint: .bottom)
+            .ignoresSafeArea()
+            
+            VStack(spacing: 50) {
+                Spacer()
+                Spacer()
+                Text("Rock-Paper-Scissors")
+                    .font(.largeTitle.bold())
+                    .foregroundColor(.white)
+                    .padding()
+                Text("Your score: \(score)")
+                    .font(.subheadline.weight(.heavy))
+                    .foregroundColor(.red)
+                Text("\(shapes[confrontingShape])")
+                    .font(.system(size: 200))
+                Text("\(winOrLose ? "- Win!" : "- Lose!")")
+                    .italic()
+                    .bold()
+                Spacer()
+                HStack {
+                    ForEach(0..<3) { emoji in
+                        Button {
+                            shapeTapped(emoji)
+                        } label: {
+                            Text("\(shapes[emoji])")
+                                .font(.system(size: 100))
+                        }
+                    }
+                }
+                Spacer()
+                Spacer()
+            }
         }
-        .padding()
+    }
+
+    func shapeTapped(_ emoji: Int) {
+        let rock = "🪨"
+        let paper = "🧻"
+        let scissors = "✂️"
+        
+        switch confrontingShape {
+        case 0:
+            if shapes[emoji] == paper {
+                winOrLose ? (score += 1) : (score -= 1)
+            } else if shapes[emoji] == scissors {
+                winOrLose ? (score -= 1) : (score += 1)
+            } else {
+                confrontingShape = Int.random(in: 0 ..< 3)
+            }
+        case 1:
+            if shapes[emoji] == rock {
+                winOrLose ? (score -= 1) : (score += 1)
+            } else if shapes[emoji] == scissors {
+                winOrLose ? (score += 1) : (score -= 1)
+            } else {
+                confrontingShape = Int.random(in: 0 ..< 3)
+            }
+        case 2:
+            if shapes[emoji] == rock {
+                winOrLose ? (score += 1) : (score -= 1)
+            } else if shapes[emoji] == paper {
+                winOrLose ? (score -= 1) : (score += 1)
+            } else {
+                confrontingShape = Int.random(in: 0 ..< 3)
+            }
+        default:
+            return
+        }
+        
+        winOrLose.toggle()
+        confrontingShape = Int.random(in: 0 ..< 3)
     }
 }
 
